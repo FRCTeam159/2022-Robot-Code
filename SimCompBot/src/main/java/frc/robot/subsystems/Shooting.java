4c9b9707dc257bee47cbd47c4233c23027ee4074
Alpine-Robotics <alpinerobotics@gmail.com>
2022-02-10 09:37:03 -0700

    sim shooter - added limit switch

--- SimCompBot/src/main/java/frc/robot/subsystems/Shooting.java
+++ SimCompBot/src/main/java/frc/robot/subsystems/Shooting.java
@@ -8,30 +8,35 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.objects.SparkMotor;
+import gazebo.SimContact;
 
 public class Shooting extends SubsystemBase implements Constants {
   /** Creates a new Intake. */
   private SparkMotor intake;
   private SparkMotor shoot;
+  private SimContact limit_switch;
   private boolean intake_is_on=false;
   private boolean shooter_is_on = false;
+  private boolean ball_captured = false;
 
   public Shooting() {
     intake = new SparkMotor(INTAKE);
     shoot = new SparkMotor(SHOOTER);
+    limit_switch=new SimContact(0);
     intake.enable();
     shoot.enable();
+    limit_switch.enable();
     setIntakeOff();
     setShooterOff();
     intake.setScale(1);
     shoot.setScale(1);
     SmartDashboard.putNumber("Intake speed", 0);
 		SmartDashboard.putNumber("Shooter speed", 0);
-
+    SmartDashboard.putBoolean("Ball Captured", ball_captured);
   }
 
   public void setShooterOn() {
-    shoot.set(-20); // rps
+    shoot.set(-10); // rps
     shooter_is_on = true;
   }
 
@@ -53,10 +58,14 @@ public class Shooting extends SubsystemBase implements Constants {
  }
  public boolean isShooterOn() {
   return shooter_is_on;
-}
+ }
+ public boolean isBallCaptured() {
+  return ball_captured;
+ }
   @Override
   public void periodic() {
     SmartDashboard.putNumber("Intake speed", intake.getRate());
 		SmartDashboard.putNumber("Shooter speed", shoot.getRate());
+    SmartDashboard.putBoolean("Ball Captured", limit_switch.inContact());
   }
 }
